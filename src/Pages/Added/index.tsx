cfabc86d7e3d7e737e290a5670d83551f27dcf9d
carloseduardodb <carloscangere@hotmail.com>
2021-05-27 03:10:55 -0300

    add components in page and organize layout

--- src/Pages/Added/index.tsx
+++ src/Pages/Added/index.tsx
@@ -1,11 +1,54 @@
-import React from "react";
+import { Form } from "@unform/mobile";
+import React, { useRef } from "react";
 import Input from "../../Components/Input";
-import { Container } from "./styles";
+import {
+  Container,
+  Title,
+  ContentAll,
+  ContentButton,
+  DrawTeacher,
+  ContentImage,
+} from "./styles";
+import DefaultButton from "../../Components/DefautButton";
+import ReturnButton from "../../Components/ReturnButton";
+import { View } from "react-native";
+
+interface propsForm {
+  name: string;
+  surname: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 const Added = () => {
+  const formRef = useRef(null);
+  function handleSubmit(data: propsForm) {
+    console.log(data);
+  }
   return (
     <Container>
-      <Input />
+      <ReturnButton />
+      <ContentAll>
+        <ContentImage>
+          <DrawTeacher
+            resizeMode="contain"
+            source={require("../../../assets/addedbg.png")}
+          />
+        </ContentImage>
+        <Title>Preencha o formulário abaixo:</Title>
+        <Input icon="bookmark" placeholder="Seu nome" />
+        <Input icon="book-open" placeholder="Seu sobrenome" />
+        <Input icon="at-sign" placeholder="Seu email" />
+        <ContentButton>
+          <DefaultButton
+            style={{ marginTop: 30 }}
+            onPress={() => {}}
+            title="Enviar"
+            color="#fff"
+          ></DefaultButton>
+        </ContentButton>
+      </ContentAll>
     </Container>
   );
 };
--- src/Pages/Added/styles.ts
+++ src/Pages/Added/styles.ts
@@ -1,3 +1,35 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background-color: #fff;
+  height: 100%;
+  padding-vertical: 10%;
+  padding-horizontal: 7%;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  margin-vertical: 20px;
+  font-weight: 700;
+  color: #423f80;
+`;
+
+export const ContentButton = styled.View`
+  align-items: flex-end;
+`;
+
+export const DrawTeacher = styled.ImageBackground`
+  padding: 50px;
+`;
+
+export const ContentImage = styled.View`
+  background-color: #eff8ff;
+  padding: 20px 0;
+  margin-top: 10px;
+  border-radius: 50px;
+`;
+
+export const ContentAll = styled.View`
+  height: 100%;
+  justify-content: space-around;
+`;
