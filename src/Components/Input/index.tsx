6644bc5a7299ac46f0e68e0e3a91610f6732966e
carloseduardodb <carloscangere@hotmail.com>
2021-05-27 03:09:34 -0300

    add Input for use in every project

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { View } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { TextInputProps, View } from "react-native";
+import { Feather as Icon } from "@expo/vector-icons";
+import { Container, Input as Ipt, ContentIcon } from "./styles";
 
-const Input = () => {
+interface Props extends TextInputProps {
+  icon?: string | any;
+}
+
+const Input: React.FC<Props> = ({ icon = "alert-circle", ...rest }) => {
   return (
-    <View>
-      <TextInput />
-    </View>
+    <Container>
+      <ContentIcon>
+        <Icon size={30} color="#30a6ff" name={icon} />
+      </ContentIcon>
+
+      <Ipt {...rest}></Ipt>
+    </Container>
   );
 };
 
