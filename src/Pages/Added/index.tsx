3eae336f93f1e67b513060421dba307f9d96fcf3
carloseduardodb <carloscangere@hotmail.com>
2021-05-28 20:26:00 -0300

    add support for unform

--- src/Pages/Added/index.tsx
+++ src/Pages/Added/index.tsx
@@ -1,6 +1,7 @@
-import { Form } from "@unform/mobile";
-import React, { useRef } from "react";
+import React, { useRef, useCallback } from "react";
 import Input from "../../Components/Input";
+import { Form } from "@unform/mobile";
+import * as Yup from "yup";
 import {
   Container,
   Title,
@@ -11,6 +12,10 @@ import {
 } from "./styles";
 import DefaultButton from "../../Components/DefaultButton";
 import ReturnButton from "../../Components/ReturnButton";
+import { FormHandles } from "@unform/core";
+import getValidationsErrors from "../../utils/getValidationsErrors";
+import api from "../../services/api";
+import { ToastAndroid } from "react-native";
 
 interface propsForm {
   name: string;
@@ -21,10 +26,46 @@ interface propsForm {
 }
 
 const Added = () => {
-  const formRef = useRef(null);
-  function handleSubmit(data: propsForm) {
-    console.log(data);
+  const formRef = useRef<FormHandles>(null);
+  async function sendData(data: propsForm) {
+    api
+      .post("teacher/create", data)
+      .then((data) => {
+        ToastAndroid.show("Dados salvos com sucesso!", 5);
+      })
+      .catch(() => {
+        ToastAndroid.show(
+          "Erro ao salvar os dados, entre em contato com suporte do sistema",
+          5
+        );
+      });
   }
+  const handleSubmit = useCallback(async (data: propsForm, { reset }) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .min(3, "Nome pequeno demais")
+          .max(150, "Máximo de 150 caracteres!"),
+        surname: Yup.string()
+          .min(3, "Sobrenome pequeno demais")
+          .max(150, "Máximo de 150 caracteres!"),
+        email: Yup.string().email("Digite um e-mail válido!"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await sendData(data);
+      reset();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationsErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+    }
+  }, []);
   return (
     <Container>
       <ReturnButton />
@@ -36,17 +77,19 @@ const Added = () => {
           />
         </ContentImage>
         <Title>Preencha o formulário abaixo:</Title>
-        <Input icon="bookmark" placeholder="Seu nome" />
-        <Input icon="book-open" placeholder="Seu sobrenome" />
-        <Input icon="at-sign" placeholder="Seu email" />
-        <ContentButton>
-          <DefaultButton
-            style={{ marginTop: 30 }}
-            onPress={() => {}}
-            title="Enviar"
-            color="#fff"
-          ></DefaultButton>
-        </ContentButton>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input name="name" icon="bookmark" placeholder="Nome" />
+          <Input name="surname" icon="book-open" placeholder="Sobrenome" />
+          <Input name="email" icon="at-sign" placeholder="Email" />
+          <ContentButton>
+            <DefaultButton
+              style={{ marginTop: 30 }}
+              onPress={() => formRef.current?.submitForm()}
+              title="Enviar"
+              color="#fff"
+            ></DefaultButton>
+          </ContentButton>
+        </Form>
       </ContentAll>
     </Container>
   );
